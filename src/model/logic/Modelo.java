85e7e9381b10f6b9ac8cf1178693559fbb1a419a
Andres Felipe Rincon Pinzon <af.rincon@sis.virtual.uniandes.edu.co>
2020-02-25 13:27:27 -0500

    quick

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -170,6 +170,45 @@ private Queue<Comparendo> datos;
 	else a[k] = aux[i++];
 	}
 	
+	private static int partition(Comparable[] a, int lo, int hi)
+	{
+	 int i = lo, j = hi+1;
+	 while (true) 
+	 {
+		 while (less(a[++i], a[lo]))
+		 if (i == hi) break;
+		 while (less(a[lo], a[--j]))
+		 if (j == lo) break;
+
+		 if (i >= j) break;
+		 exch(a, i, j);
+		 }
+		 exch(a, lo, j);
+		 return j;
+		}
+	
+	public static void exch(Comparable[] a, int i, int j)
+	{
+		Comparable temporal = a[i];
+		a[i] = a[j];
+		a[j] = temporal;		
+	}
+	
+	
+	 public static void mergesort(Comparable[] a)
+	 {
+	 crearArreglo().shuffle(a);
+	 sort(a, 0, a.length - 1);
+	 }
+	 private static void sortTwo(Comparable[] a, int lo, int hi)
+	 {
+	 if (hi <= lo) return;
+	 int j = partition(a, lo, hi);
+	 sort(a, lo, j-1);
+	 sort(a, j+1, hi);
+	 }
+	
+	
 	public static  boolean less(Comparable a, Comparable b)
 	{
 		return a.compareTo(b)<0;
