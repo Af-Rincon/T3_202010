14e95c93c73e36c8c118e4d1d3cc9455d6361fd6
Andres Felipe Rincon Pinzon <af.rincon@sis.virtual.uniandes.edu.co>
2020-02-26 17:08:49 -0500

    arreglos

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -21,11 +21,11 @@ import model.data_structures.Queue;
  *
  */
 public class Modelo {
-private Queue<Comparendo> datos;
-	
+	private Queue<Comparendo> datos;
+
 	public static String PATH = "./data/comparendos_dei_2018.geojson";
 	private static Comparable[] aux;
-	
+
 	/**
 	 * Constructor del modelo del mundo con capacidad predefinida
 	 */
@@ -33,7 +33,7 @@ private Queue<Comparendo> datos;
 	{
 		datos = new Queue<Comparendo>();
 	}
-	
+
 	/**
 	 * Servicio de consulta de numero de elementos presentes en el modelo 
 	 * @return numero de elementos presentes en el modelo
@@ -42,7 +42,7 @@ private Queue<Comparendo> datos;
 	{
 		return datos.darTamano();
 	}
-	
+
 	public Queue<Comparendo> repetidos()
 	{
 		if(datos==null)
@@ -75,38 +75,38 @@ private Queue<Comparendo> datos;
 
 		return mayor;
 	}
-	
-	
+
+
 
 	public Comparendo darPrimeroCola()
 	{
 		return datos.darPrimerElemento();
 	}
-	
+
 	public Comparendo darUltimoCola()
 	{
 		return datos.darUltimoElemento();
 	}
-	
+
 	public void cargar()
 	{
 		JsonReader reader;
-		
+
 		try
 		{
 			reader = new JsonReader(new FileReader(PATH));
 			JsonElement elem = JsonParser.parseReader(reader);
 			JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
-			
-			
+
+
 			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
 
 			for(JsonElement e: e2) {
 				int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
-				
+
 				String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();	
 				Date FECHA_HORA = parser.parse(s); 
-				
+
 				String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETE").getAsString();
 				String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHI").getAsString();
 				String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVI").getAsString();
@@ -116,20 +116,20 @@ private Queue<Comparendo> datos;
 
 				double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
 						.get(0).getAsDouble();
-				
+
 				double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
 						.get(1).getAsDouble();
 
 				Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, DES_INFRAC, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION, LOCALIDAD, longitud, latitud);
 				datos.enqueue(c);
+			}
 		}
-	}
 		catch (FileNotFoundException | ParseException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}	
 	}
-	
+
 	public Comparendo[] copiarArreglo()
 	{
 		Comparendo[] comparendos = new Comparendo[datos.darTamano()];
@@ -141,116 +141,118 @@ private Queue<Comparendo> datos;
 		}
 		return comparendos;
 	}
-	
+
+	//Merge
 	public void mergeSort()
 	{
 		sort(copiarArreglo());	
 	}
-	
+
 	// auxiliary array for merges
 	public static void sort(Comparable[] a) {
-	aux = new Comparable[a.length];
-	sort(a, 0, a.length - 1); // Allocate space just once.
+		aux = new Comparable[a.length];
+		sort(a, 0, a.length - 1); // Allocate space just once.
 	}
-	
+
 	private static void sort(Comparable[] a, int lo, int hi) {
-	if (hi <= lo) return; // Sort a[lo..hi].
-	int mid = lo + (hi - lo)/2;
-	sort(a, lo, mid);
-	// Sort left half.
-	sort(a, mid+1, hi);
-	// Sort right half.
-	merge(a, lo, mid, hi);
+		if (hi <= lo) return; // Sort a[lo..hi].
+		int mid = lo + (hi - lo)/2;
+		sort(a, lo, mid);
+		// Sort left half.
+		sort(a, mid+1, hi);
+		// Sort right half.
+		merge(a, lo, mid, hi);
 	}
-	
+
 	public static void merge(Comparable[] a, int lo, int mid, int hi)
 	{
-	// Merge a[lo..mid] with a[mid+1..hi].
-	int i = lo, j = mid+1;
-	for (int k = lo; k <= hi; k++)
-	// Copy a[lo..hi] to aux[lo..hi].
-	aux[k] = a[k];
-	for (int k = lo; k <= hi; k++)
-	// Merge back to a[lo..hi].
-	if (i > mid) a[k] = aux[j++];
-	else if (j > hi ) a[k] = aux[i++];
-	else if (less(aux[j], aux[i])) a[k] = aux[j++];
-	else a[k] = aux[i++];
+		// Merge a[lo..mid] with a[mid+1..hi].
+		int i = lo, j = mid+1;
+		for (int k = lo; k <= hi; k++)
+			// Copy a[lo..hi] to aux[lo..hi].
+			aux[k] = a[k];
+		for (int k = lo; k <= hi; k++)
+			// Merge back to a[lo..hi].
+			if (i > mid) a[k] = aux[j++];
+			else if (j > hi ) a[k] = aux[i++];
+			else if (less(aux[j], aux[i])) a[k] = aux[j++];
+			else a[k] = aux[i++];
 	}
-	
+
+	//Quick
+
+
+	public static void quickSort(Comparable[] a)
+	{
+		shuffle(a);
+		sortTwo(a, 0, a.length - 1);
+	}
+
+	private static void sortTwo(Comparable[] a, int lo, int hi)
+	{
+		if (hi <= lo) return;
+		int j = partition(a, lo, hi);
+		sort(a, lo, j-1);
+		sort(a, j+1, hi);
+	}
+
 	private static int partition(Comparable[] a, int lo, int hi)
 	{
-	 int i = lo, j = hi+1;
-	 while (true) 
-	 {
-		 while (less(a[++i], a[lo]))
-		 if (i == hi) break;
-		 while (less(a[lo], a[--j]))
-		 if (j == lo) break;
+		int i = lo, j = hi+1;
+		while (true) 
+		{
+			while (less(a[++i], a[lo]))
+				if (i == hi) break;
+			while (less(a[lo], a[--j]))
+				if (j == lo) break;
 
-		 if (i >= j) break;
-		 exch(a, i, j);
-		 }
-		 exch(a, lo, j);
-		 return j;
+			if (i >= j) break;
+			exch(a, i, j);
 		}
-	
+		exch(a, lo, j);
+		return j;
+	}
+
 	public static void exch(Comparable[] a, int i, int j)
 	{
 		Comparable temporal = a[i];
 		a[i] = a[j];
 		a[j] = temporal;		
 	}
-	
-	
-	 public static void mergesort(Comparable[] a)
-	 {
-	 shuffle(a);
-	 sort(a, 0, a.length - 1);
-	 }
-	 
-	 private static void shuffle(Comparable[] a)
-	 {
-		 Random rd = new Random();
-		 
-		 for(int i = a.length -1; i>0;i--)
-		 {
-			 int index = rd.nextInt(i+1);
-			 Comparable n = a[index];
-			 a[index] = a[i];
-			 a[i] = n;
-		 }
-	 }
-	 
-	 private static void sortTwo(Comparable[] a, int lo, int hi)
-	 {
-	 if (hi <= lo) return;
-	 int j = partition(a, lo, hi);
-	 sort(a, lo, j-1);
-	 sort(a, j+1, hi);
-	 }
-	
-	 public class Shell
-	 {
-	 public void sortSS(Comparable[] a)
-	 {
-		 int N = a.length;
-		 int h = 1;
-		 while (h < N/3)
-			 h = 3*h + 1;
-		 while (h >= 1) {
-			 for (int i = h; i < N; i++) {
-				 for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
-					 exch(a, j, j-h);
-			 }
-			 h = h/3;
-		 }
-	 }
-	 }
+
+	private static void shuffle(Comparable[] a)
+	{
+		Random rd = new Random();
+
+		for(int i = a.length -1; i>0;i--)
+		{
+			int index = rd.nextInt(i+1);
+			Comparable n = a[index];
+			a[index] = a[i];
+			a[i] = n;
+		}
+	}
+
+	//Shell
+	public void shellSort(Comparable[] a)
+	{
+		int N = a.length;
+		int h = 1;
+		while (h < N/3)
+			h = 3*h + 1;
+		while (h >= 1) {
+			for (int i = h; i < N; i++) {
+				for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+					exch(a, j, j-h);
+			}
+			h = h/3;
+		}
+	}
+
 
 	public static  boolean less(Comparable a, Comparable b)
 	{
 		return a.compareTo(b)<0;
 	}
-	
+
 }
